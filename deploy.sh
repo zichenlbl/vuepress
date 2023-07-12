540900832de5c04960ef0d57a2aa267412aebfb6
zichenlbl <zichen1016@qq.com>
2023-07-13 00:00:17 +0800

    修改部署生成文件同步代码地址，由ssh方式改为http方式

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,8 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:zichenlbl/vuepress.git master:gh-pages
+# https://github.com/zichenlbl/vuepress.git
+# git push -f git@github.com:zichenlbl/vuepress.git master:gh-pages
+git push -f https://github.com/zichenlbl/vuepress.git master:gh-pages
 
 cd -
